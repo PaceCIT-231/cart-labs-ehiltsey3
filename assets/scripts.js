2be7648a832b53a23dbea38d29d2066c8136ed65
ehiltsey3 <ehiltsey3@gmail.com>
2022-11-14 10:49:54 -0500

    Completed lab 8.

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,23 +1,35 @@
 let currentPrice=0, itemCount=0
 
 function addToCart(cookie) {
-    /* 
-        PRICES
+
+    /*
         peanut butter: 20
         sandies: 30
         party press: 35
         chocolate chip: 25
     */
-   
-   console.log('The user is adding this type of cookie to their cart: ' , cookie) 
 
-   //add 1 to the itemCount variable
+    console.log('The user is adding this type of cookie to their cart: ' , cookie) 
+
+    console.log(itemCount++) //adds 1 to the itemCount variable and displays in the console 
+
+    if(cookie == 'peanut butter')
+        currentPrice = currentPrice + 20;
+    if(cookie == 'sandies')
+        currentPrice = currentPrice + 30;
+    if(cookie == 'party press')
+        currentPrice = currentPrice + 35;
+    if(cookie == 'chocolate chip')
+        currentPrice = currentPrice + 25;
+    console.log(currentPrice);
    //add the correct price to the currentPrice variable
-
+    
 }
 
 function checkout() {
     console.log('User is checking out.')
+    alert('You have ' + itemCount + ' item(s) in your cart. Your total cost is $' + currentPrice + '.');
+    //alert('The total cost is $' + currentPrice + '.');
     //Let your customer know how many items they are purchasing and the price
     
 }
