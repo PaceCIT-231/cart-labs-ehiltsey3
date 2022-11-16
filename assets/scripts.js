9b1e0ae777aeafbfccce925a5ed119e9f3a4e07b
ehiltsey3 <ehiltsey3@gmail.com>
2022-11-16 14:57:14 -0500

    Completed Lab 9.

--- assets/scripts.js
+++ assets/scripts.js
@@ -11,7 +11,10 @@ function addToCart(cookie) {
 
     console.log('The user is adding this type of cookie to their cart: ' , cookie) 
 
-    console.log(itemCount++) //adds 1 to the itemCount variable and displays in the console 
+    //document.getElementById("cartItems").innerText = itemCount;
+    console.log(itemCount++); //adds 1 to the itemCount variable and displays in the console 
+    document.getElementById("cartItems").innerText = itemCount;
+    
 
     if(cookie == 'peanut butter')
         currentPrice = currentPrice + 20;
@@ -21,15 +24,32 @@ function addToCart(cookie) {
         currentPrice = currentPrice + 35;
     if(cookie == 'chocolate chip')
         currentPrice = currentPrice + 25;
+    
     console.log(currentPrice);
+
+    document.querySelector("span").innerHTML = "$" + currentPrice;
    //add the correct price to the currentPrice variable
     
 }
 
 function checkout() {
     console.log('User is checking out.')
-    alert('You have ' + itemCount + ' item(s) in your cart. Your total cost is $' + currentPrice + '.');
-    //alert('The total cost is $' + currentPrice + '.');
+    prompt("Enter your name:" + '\n' + "Enter your email:");
+    currentPrice = 0;
+    itemCount = 0;
+
+    document.querySelector("span").innerHTML = "$" + currentPrice;
+    document.getElementById("cartItems").innerText = itemCount;
+    
+    //alert('You have ' + itemCount + ' item(s) in your cart. Your total cost is $' + currentPrice + '.');
+    
     //Let your customer know how many items they are purchasing and the price
     
+}
+
+function darkMode() {
+	// add your code here
+    document.querySelector("body").style.backgroundColor = "black";
+    document.querySelector("body").style.color = "white";
+
 }
